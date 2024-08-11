09ea65f15f93c68a53e29dd3aded5b1b3d6a1a64
Rahul Krishna K A <rahulkrishna7602@gmail.com>
2024-08-12 01:08:15 +0530

    Bug fixes

--- app/(authenticated)/feedback/page.tsx
+++ app/(authenticated)/feedback/page.tsx
@@ -26,6 +26,7 @@ import { useQuery } from "@tanstack/react-query";
 import Error from "@/components/Error";
 import Loading from "@/components/Loading";
 import Refetching from "@/components/Refetching";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function Page({
   params,
@@ -35,6 +36,7 @@ export default function Page({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const session = useSession();
+  const queryClient = useQueryClient();
 
   interface cohortColumn {
     id: number;
@@ -119,25 +121,35 @@ export default function Page({
   const [cohortAdd, setCohortAdd] = useState(false);
   const [partnerAdd, setPartnerAdd] = useState(true);
 
-  const AddCohort = (selectedCohort: cohortColumn) => {
+  const AddCohort = async (selectedCohort: cohortColumn) => {
     if (cohortData.data) {
       cohortData.data.forEach((element) => {
         if (element.name === selectedCohort.name) {
           setRecipientCohorts.setState([element]);
           setRecipientCohortCount(1);
+          setRecipientPartners.setState([]);
+          setRecipientPartnerCount(0);
         }
       });
+      // queryClient.invalidateQueries({
+      //   queryKey: ["notifDataCohort"],
+      // });
     }
   };
 
-  const AddPartner = (selectedPartner: poc) => {
+  const AddPartner = async (selectedPartner: poc) => {
     if (partnerData.data) {
       partnerData.data.forEach((element) => {
         if (element.user_id.name === selectedPartner.user_id.name) {
           setRecipientPartners.setState([element]);
           setRecipientPartnerCount(1);
+          setRecipientCohorts.setState([]);
+          setRecipientCohortCount(0);
         }
       });
+      // queryClient.invalidateQueries({
+      //   queryKey: ["notifDataPoc"],
+      // });
     }
   };
 
@@ -218,7 +230,7 @@ export default function Page({
   // });
 
   const notifDataPoc = useQuery<notif[]>({
-    queryKey: ["notifDataCohort"],
+    queryKey: ["notifDataPoc"],
     queryFn: async () => {
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_BASE_URL}/notification/poc/get/${recipientPartners[0]?.id}`,
@@ -279,7 +291,7 @@ export default function Page({
         </TabsContent>
         <TabsContent value="search">
           <div className="overflow-x-auto px-1 pt-2">
-            <div className="flex w-full flex-col items-start rounded-md border px-3 py-3">
+            <div className="flex w-full flex-col items-start rounded-md border px-1 py-1">
               <div className="flex w-full flex-row px-1 py-1 items-center justify-between">
                 <Popover open={open} onOpenChange={setOpen}>
                   <PopoverTrigger asChild>
@@ -287,7 +299,7 @@ export default function Page({
                       variant="outline"
                       role="combobox"
                       aria-expanded={open}
-                      className="lg:w-fit w-full font-light py-3 rounded-lg px-3  mb-2 flex justify-center items-center mr-2"
+                      className="lg:w-fit w-full font-light py-3 rounded-lg px-3 flex justify-center items-center mr-2"
                     >
                       <div className="mx-2">
                         {recipientCohortCount != 0 && (
@@ -318,7 +330,11 @@ export default function Page({
                                       key={partner.id}
                                       value={partner.user_id.name}
                                       onSelect={(currentValue) => {
-                                        AddPartner(partner);
+                                        AddPartner(partner).then(() => {
+                                          queryClient.invalidateQueries({
+                                            queryKey: ["notifDataPoc"],
+                                          });
+                                        });
                                         setOpen(false);
                                       }}
                                     >
@@ -347,7 +363,11 @@ export default function Page({
                                       key={cohort.id}
                                       value={cohort.name}
                                       onSelect={(currentValue) => {
-                                        AddCohort(cohort);
+                                        AddCohort(cohort).then(() => {
+                                          queryClient.invalidateQueries({
+                                            queryKey: ["notifDataCohort"],
+                                          });
+                                        });
                                         setOpen(false);
                                       }}
                                     >
@@ -365,7 +385,7 @@ export default function Page({
                 <ToggleGroup
                   type="single"
                   defaultValue="partners"
-                  className=" ml-1 gap-0 mb-2 rounded bg-slate-200"
+                  className=" ml-1 gap-0 rounded bg-slate-200"
                 >
                   <ToggleGroupItem
                     className=" px-2 font-light rounded-l-sm rounded-r-none data-[state=on]:bg-primary data-[state=on]:text-white"
@@ -392,6 +412,7 @@ export default function Page({
               {!notifDataCohort.isError &&
                 !notifDataCohort.isLoading &&
                 notifDataCohort.data &&
+                recipientCohortCount == 1 &&
                 notifDataCohort.data.constructor === Array && (
                   <div>
                     <NotificationTable
@@ -416,6 +437,7 @@ export default function Page({
               {!notifDataPoc.isError &&
                 !notifDataPoc.isLoading &&
                 notifDataPoc.data &&
+                recipientPartnerCount == 1 &&
                 notifDataPoc.data.constructor === Array && (
                   <div>
                     <NotificationTable
